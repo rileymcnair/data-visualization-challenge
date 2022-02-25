1eb006e711bf30e234cc357b20445594058912b9
Riley McNair <riley.mcnair7@gmail.com>
2022-02-24 23:57:11 -0800

    layouts with bootstrap

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express")
 const app = express()
+const expressLayouts = require('express-ejs-layouts')
 const PORT = 4000
 
 //middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(express.static(__dirname + 'public'))
+app.use(express.static('public'))
+
+app.use(expressLayouts)
+app.set('layout', './layouts/main')
+app.set('view engine', 'ejs')
 
 
-const routes = require('./routes/index.js')
-
+app.get('/', (req, res) => {
+    res.render('index')
+})
+// const routes = require('./routes/routes.js')
+// app.use('/', routes)
 
 app.listen(PORT, () => {
     console.log(`Express is running on port ${PORT}`)
