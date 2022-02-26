226142b5d02d99bf6376eab048a11eee72f1e37e
Riley McNair <riley.mcnair7@gmail.com>
2022-02-26 03:35:04 -0800

    weather api integration

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const PORT = 4000
+const axios = require('axios')
 
 //middleware
 app.use(express.json())
@@ -15,6 +16,9 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('index')
+    axios.get('https://www.metaweather.com/api/location/44418')
+    .then(res => console.log(res.data.consolidated_weather))
+    .catch(err => console.log(err))
 })
 // const routes = require('./routes/routes.js')
 // app.use('/', routes)
